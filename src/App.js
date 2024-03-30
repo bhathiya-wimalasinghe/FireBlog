9cb16964b8fab84c0cb73e1554e6a1ad5e5b9b0d
bhathiya-wimalasinghe <bhathiyawimalasinghe@gmail.com>
2024-03-30 19:28:03 +0530

    implement footer component ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TopBar from "./components/TopBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import logo from "../images/FireBlog.svg";
+import { IconButton, Stack } from "@mui/material";
+import { GitHub, LinkedIn, Twitter, X } from "@mui/icons-material";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="http://www.linkedin.com/in/bhathiya-wimalasinghe"
+      >
+        Bhathiya Wimalasinghe
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {". All Rights Reserved."}
+    </Typography>
+  );
+}
+
+const logoStyle = {
+  width: "140px",
+  height: "auto",
+  cursor: "pointer",
+};
+
+export default function Footer() {
+  return (
+    <Box mt={2} component="footer" sx={{ bgcolor: "lightgray", py: 2 }}>
+      <Box display="flex" justifyContent="space-around" alignItems="center">
+        <Box>
+          <img alt="fireblog logo" src={logo} style={logoStyle} />
+        </Box>
+        <Box display="flex" alignItems="center" flexDirection="column">
+          <Typography variant="h6">Follow Us</Typography>
+          <Stack direction="row">
+            <IconButton
+              aria-label="GitHub"
+              href="http://www.github.com/bhathiya-wimalasinghe"
+            >
+              <GitHub />
+            </IconButton>
+            <IconButton aria-label="X" href="https://twitter.com/Bhathiya_1996">
+              <X />
+            </IconButton>
+            <IconButton
+              aria-label="LinkedIn"
+              href="http://www.linkedin.com/in/bhathiya-wimalasinghe"
+            >
+              <LinkedIn />
+            </IconButton>
+          </Stack>
+        </Box>
+      </Box>
+
+      <Copyright />
+    </Box>
+  );
+}
